b6d5458a43800308402f8f451e49e2b77902ec69
Matthew Foyle <mfoyle93@gmail.com>
2022-12-05 16:20:17 +0000

    style: quick style cleanup

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,21 +1,23 @@
-import { useSearchBox, useHits } from 'react-instantsearch-hooks-web'
+import { useSearchBox, useHits } from "react-instantsearch-hooks-web"
 
-import { CustomRefinementList } from './customRefinementList'
+import { CustomRefinementList } from "./customRefinementList"
 
 function SearchPage() {
   const { refine } = useSearchBox()
-  refine('bag')
+  refine("bag")
 
   const { hits } = useHits()
 
   return (
     <div className="srp">
       <div className="facets">
+        <h2>Refinement List</h2>
         <CustomRefinementList attribute="brand" />
       </div>
       <div className="hits">
+        <h2>Hits</h2>
         {hits.map((hit) => (
-          <p>{JSON.stringify(hit)}</p>
+          <div>{hit.name}</div>
         ))}
       </div>
     </div>
